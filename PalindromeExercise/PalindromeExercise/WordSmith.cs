b7485df88e8f4b76ece5c084c8187ba7259f3f8e
Jeffrey Baker <jeffrey1871@gmail.com>
2021-04-28 12:19:01 -0500

    finished

---
+++ PalindromeExercise/PalindromeExercise/WordSmith.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PalindromeExercise
+{
+    public class WordSmith
+    {
+        public static bool IsAPalindrome(string s)
+        {
+            char[] c = s.ToCharArray();
+            Array.Reverse(c);
+            string revS = new String(c);
+            return s == revS;
+        }
+    }
+}
--- PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
+++ PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
@@ -1,14 +1,19 @@
 using System;
 using Xunit;
+using PalindromeExercise;
 
 namespace PalindromeExerciseTests
 {
     public class UnitTest1
     {
-        [Fact]
-        public void Test1()
+        [Theory]
+        [InlineData("racecar", true)]
+        [InlineData("bowling", false)]
+        public void IsAPalindrome(string s, bool expected)
         {
+            var actual =  WordSmith.IsAPalindrome(s);
 
+            Assert.Equal(actual, expected);
         }
     }
 }
